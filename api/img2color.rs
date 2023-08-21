2fb2d189a90a85b875dff21dbe7c0fe718d32dfe
yxlr123 <dbh2858304517@gmail.com>
2023-08-21 11:11:23 +0800

    数据库缓存

--- api/img2color.rs
+++ api/img2color.rs
@@ -1,23 +1,46 @@
 use std::collections::HashMap;
-use std::io;
-use std::sync::{Arc, Mutex};
-use std::thread;
+use std::env;
 
-use hyper::{Body as hBody, Client, Request as hRequest, Uri};
-use hyper_tls::HttpsConnector;
-use image::{io::Reader as ImageReader, DynamicImage, GenericImageView,imageops::FilterType};
+use md5;
+use reqwest::{self,Url};
+use image::{self, DynamicImage, GenericImageView,imageops::FilterType};
 use palette::LinSrgb;
-use num_cpus;
 use serde_json::json;
-use url::Url;
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
+use mongodb::{Client, options::{ClientOptions,ServerApi, ServerApiVersion},bson::doc};
+use serde::{Deserialize,Serialize};
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     run(handler).await
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct Img {
+    hex:String,
+    color:String
+}
+
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
+    dotenv().ok();
+    let username = env::var("USERNAME").unwrap(); 
+    let password = env::var("PASSWORD").unwrap();
+    let host = env::var("MONGO_HOST").unwrap();
+
+    let url = format!(
+        "mongodb+srv://{}:{}@{}/?retryWrites=true&w=majority", 
+        username, password, host
+    );
+
+    // 创建客户端
+    let mut client_options = ClientOptions::parse(&url).await?;
+    let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
+    client_options.server_api = Some(server_api);
+    let db_client = Client::with_options(client_options)?;
+    let db = db_client.database("admin");
+    let collection = db.collection::<Img>("colors");
+
     let parsed_url = Url::parse(&req.uri().to_string()).unwrap();
     let hash_query: HashMap<String, String> = parsed_url.query_pairs().into_owned().collect();
     let img = hash_query.get("img");
@@ -40,8 +63,9 @@ pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
         }
     };
     let img: DynamicImage;
+    let img_hex: String;
     match download_image_and_parse(img_url).await {
-        Ok(i) => img = i,
+        Ok(i) => (img,img_hex) = i,
         Err(e) => {
             return Ok(Response::builder()
                 .status(StatusCode::INTERNAL_SERVER_ERROR)
@@ -55,12 +79,28 @@ pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
                 )?)
         }
     }
+    let cursor = collection.find(doc! {"hex":&img_hex}, None).await?;
+    if let Ok(i) = cursor.deserialize_current() {
+        return Ok(Response::builder()
+            .status(StatusCode::OK)
+            .header("Content-Type", "application/json")
+            .body(
+                json!({
+                    "RGB": &i.color
+                })
+                .to_string()
+                .into(),
+            )?
+        );
+    }
+    let img = Img {hex: img_hex,color: get_theme_color(&img).await};
+    collection.insert_one(&img,None).await?;
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
         .body(
             json!({
-              "RGB": get_theme_color(&img).await
+              "RGB": &img.color
             })
             .to_string()
             .into(),
@@ -77,93 +117,37 @@ async fn fix_url(url: &str) -> String {
 
 async fn download_image_and_parse(
     url: &str,
-) -> Result<DynamicImage, Box<dyn std::error::Error + Send + Sync>> {
-    // 将URL解析为Uri类型
+) -> Result<(DynamicImage,String), Box<dyn std::error::Error + Send + Sync>> {
     let url = fix_url(url).await;
-    let uri = url.parse::<Uri>()?;
-
-    // 创建一个新的hyper客户端
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, hyper::Body>(https);
-
-    // 创建一个新的请求
-    let request = hRequest::builder()
-        .uri(uri)
-        .header("User-Agent", "Mozilla/5.0")
-        .body(hBody::empty())?;
-
-    // 发送请求并等待响应
-    let response = client.request(request).await?;
-    if response.status() == 404 {
-        return Err(Box::new(io::Error::new(
-            io::ErrorKind::NotFound,
-            "图片不存在",
-        )));
-    }
-    // 从响应中提取字节流
-    let bytes = hyper::body::to_bytes(response.into_body()).await?;
-
-    // 使用image库解析字节流中的图像
-    let img = ImageReader::new(std::io::Cursor::new(bytes))
-        .with_guessed_format()?
-        .decode()?;
-
-    Ok(img)
+    let resp = reqwest::get(url).await?;
+    let bytes = resp.bytes().await?;
+    let img_hex = md5::compute(&bytes);
+    let img = image::load_from_memory(&bytes)?;
+    let img= img.resize(50, (img.height()*50)/img.width(), FilterType::Lanczos3);
+    Ok((img,format!("{:?}",img_hex)))
 }
 
 async fn get_theme_color(img: &DynamicImage) -> String {
-    // Resize the image to 50 pixels width
-    let img = img.resize(50, (img.height() * 50) / img.width(), FilterType::Lanczos3);
-
     // Get the image dimensions
     let (width, height) = img.dimensions();
+    // Calculate the average color of the image
+    let mut sum_red: u32 = 0;
+    let mut sum_green: u32 = 0;
+    let mut sum_blue: u32 = 0;
 
-    // Calculate the sum of RGB values of each pixel in parallel
-    let sum_rgb = Arc::new(Mutex::new((0u32, 0u32, 0u32)));
-    let pixels_per_thread = (width * height) / num_cpus::get() as u32;
-    let mut handles = Vec::new();
-
-    for tid in 0..num_cpus::get() {
-        let sum_rgb = Arc::clone(&sum_rgb);
-        let img = img.clone();
-        let start = tid * pixels_per_thread as usize;
-        let end = if tid == num_cpus::get() - 1 {
-            width * height
-        } else {
-            (tid + 1) as u32 * pixels_per_thread
-        };
-        let handle = thread::spawn(move || {
-            let mut sum_red = 0u32;
-            let mut sum_green = 0u32;
-            let mut sum_blue = 0u32;
-
-            for p in start..end as usize {
-                let x = p % width as usize;
-                let y = p / width as usize;
-                let pixel = img.get_pixel(x as u32, y as u32);
-                sum_red += pixel[0] as u32;
-                sum_green += pixel[1] as u32;
-                sum_blue += pixel[2] as u32;
-            }
-
-            let mut sum_rgb = sum_rgb.lock().unwrap();
-            sum_rgb.0 += sum_red;
-            sum_rgb.1 += sum_green;
-            sum_rgb.2 += sum_blue;
-        });
-        handles.push(handle);
+    for x in 0..width {
+        for y in 0..height {
+            let pixel = img.get_pixel(x, y);
+            sum_red += pixel[0] as u32;
+            sum_green += pixel[1] as u32;
+            sum_blue += pixel[2] as u32;
+        }
     }
 
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    // Calculate the average RGB value
     let pixel_count = (width * height) as f32;
-    let sum_rgb = sum_rgb.lock().unwrap();
-    let avg_red = (sum_rgb.0 as f32 / pixel_count).round() as u8;
-    let avg_green = (sum_rgb.1 as f32 / pixel_count).round() as u8;
-    let avg_blue = (sum_rgb.2 as f32 / pixel_count).round() as u8;
+    let avg_red = (sum_red as f32 / pixel_count).round() as u8;
+    let avg_green = (sum_green as f32 / pixel_count).round() as u8;
+    let avg_blue = (sum_blue as f32 / pixel_count).round() as u8;
 
     // Create a palette color from the average color
     let avg_color = LinSrgb::new(
